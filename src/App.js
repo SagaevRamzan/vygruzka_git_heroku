2fe7a979544e56690b7b5b769f6ae2b876a59294
SagaevRamzan <80630411+SagaevRamzan@users.noreply.github.com>
2021-06-06 11:43:31 +0300

    my commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './actions'
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {loadTodos} from "./actions";
 
 function App() {
+    const  todos=useSelector(state=>state.todos);
+    const  loading=useSelector(state=>state.loading);
+    const dispatch=useDispatch();
+
+
+    useEffect(()=>{
+        dispatch(loadTodos())
+    },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="main_title">Список дел</div>
+        <div className="title_cont zagolovok">
+            <div className="todo_id">id</div>
+            <div className="todo_title">title</div>
+            <div className="todo_body">body</div>
+        </div>
+        {
+            (loading?'идет загрузка...':todos.map(todo=>{
+                return (
+                    <div className="title_cont">
+                        <div className="todo_id">{todo.id}</div>
+                        <div className="todo_title">{todo.title}</div>
+                        <div className="todo_body">{todo.body}</div>
+                    </div>
+                )
+            }))
+
+        }
+
     </div>
   );
 }
